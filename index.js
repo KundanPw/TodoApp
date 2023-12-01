606d4ff14cb4812159a7249918d0ff4fe3cbce97
Kundan <kk9797307@gmail.com>
2023-12-01 23:11:24 +0530

    Completed todos app

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ let todos = [];
 
 let todoDataList = document.getElementById("todo-data-list");
 let saveButton = document.getElementById("save-todo");
-let todoInputBar = document.getElementById("todo-input-bar")
+let todoInputBar = document.getElementById("todo-input-bar");
+let getPendingTodosButton = document.getElementById("get-todos");
+
+getPendingTodosButton.addEventListener("click", () => {
+    todos = todos.filter((todo) => todo.status != "Finished");
+    reRenderTodos();
+
+})
 
 todoInputBar.addEventListener("keyup", function toggleSaveButton() {
     let todotext = todoInputBar.value;
@@ -67,6 +74,29 @@ function finishTodo(event) {
     reRenderTodos();
 }
 
+function editTodo(event) { 
+    let editButtonPressed = event.target;
+    let indexToEdit = Number(editButtonPressed.getAttribute("todo-idx"));
+    let detailDiv = document.querySelector(`div[todo-idx="${indexToEdit}"]`);
+    let input = document.querySelector(`input[todo-idx="${indexToEdit}"]`);
+    detailDiv.style.display = "none";
+    input.type = "text";
+    input.value = detailDiv.textContent;
+}
+
+function saveEdittedTodo(event) {
+    let input = event.target;
+    let indexToEdit = Number(input.getAttribute("todo-idx"));
+    let detailDiv = document.querySelector(`div[todo-idx="${indexToEdit}"]`);
+
+    if(event.keyCode == 13) {
+        detailDiv.textContent = input.value;
+        detailDiv.style.display = "block";
+        input.value = '';
+        input.type = "hidden";
+    }
+}
+
 function addTodo(todo, todoCount) {
     console.log("called add todo")
     let rowDiv = document.createElement("div");
@@ -77,6 +107,8 @@ function addTodo(todo, todoCount) {
     let todoActions = document.createElement("div");
     let deleteButton = document.createElement("button");
     let finishedButton = document.createElement("button");
+    let editButton = document.createElement("button");
+    let hiddenInput = document.createElement("input");
     let hr = document.createElement("hr");
 
     // adding classes
@@ -88,24 +120,39 @@ function addTodo(todo, todoCount) {
     todoActions.classList.add("todo-actions", "d-flex", "justify-content-start", "gap-2");
     deleteButton.classList.add("btn", "btn-danger", "delete-todo");
     finishedButton.classList.add("btn", "btn-success", "finish-todo");
+    editButton.classList.add("btn", "btn-warning", "edit-todo");
+    hiddenInput.classList.add("form-control", "todo-detail");
 
+    //adding attributes
     finishedButton.setAttribute("todo-idx", todoCount-1);
     deleteButton.setAttribute("todo-idx", todoCount-1);
+    editButton.setAttribute("todo-idx", todoCount-1);
+    todoDetail.setAttribute("todo-idx", todoCount-1);
+    hiddenInput.setAttribute("todo-idx", todoCount-1);
+    hiddenInput.type = "hidden";
+
+
+    //adding click listener
     deleteButton.onclick = removeTodo;
     finishedButton.onclick = finishTodo;
-
+    editButton.onclick = editTodo;
+    hiddenInput.addEventListener("keypress",saveEdittedTodo);
 
     todoNumber.textContent = `${todoCount}.`;
     todoDetail.textContent = todo.text; // sets the todo text sent from the input element
     todoStatus.textContent = todo.status;
     deleteButton.textContent = "Delete";
     finishedButton.textContent = todo.finishButtontext;
+    editButton.textContent = "Edit";
 
+    // creating the div on dom
     todoActions.appendChild(deleteButton);
     todoActions.appendChild(finishedButton);
+    todoActions.appendChild(editButton);
 
     todoItem.appendChild(todoNumber);
     todoItem.appendChild(todoDetail);
+    todoItem.appendChild(hiddenInput);
     todoItem.appendChild(todoStatus);
     todoItem.appendChild(todoActions);
 
